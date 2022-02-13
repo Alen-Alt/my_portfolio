236ea3c94cfd126f31881c816109152b6b54ad19
mac-A <>
2022-02-13 17:51:27 +0500

    Task 2.3
    1- Created a proper responsive navbar with toggle functionality using jquery.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hi, Welcome to Alen Begzic Portfolio!");
+
+function toggleNavigation() {
+	let navElement = $("#myTopnav");
+	if (navElement[0].className === "topnav") {
+		navElement[0].className += " responsive";
+	} else {
+		navElement[0].className = "topnav";
+	}
+}
